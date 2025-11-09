5cc1677542e951093184ee5a9ca5e50fcc7de0c3
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-09 09:32:11 +0100

    Update connectDatabase.js

--- src/config/connectDatabase.js
+++ src/config/connectDatabase.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 import process from "process";
 
-
 const connectDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI).p;
-        console.log("Database connected successfully! ✅");
-    } catch (error) {
-        console.log("Database connection failed! ❌", error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("Database connected successfully! ✅");
+  } catch (error) {
+    console.error("Database connection failed! ❌", error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDatabase;
