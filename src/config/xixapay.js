f5a2109c43f0c17422d2f859d78a400160c172ba
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-11-12 04:07:19 +0100

    Update xixapay.js

--- src/config/xixapay.js
+++ src/config/xixapay.js
@@ -1,14 +1,14 @@
-import axios from "axios"
-import process from "process"
+import axios from "axios";
+import process from "process";
 
-const SECRET = process.env.XIXAPAY_SECRET
-const APIKEY = process.env.XIXAPAY_API_KEY
+const SECRET = process.env.XIXAPAY_SECRET;
+const APIKEY = process.env.XIXAPAY_API_KEY;
 
 export const xixaApi = axios.create({
-    baseURL: " https://api.xixapay.com/api/v1",
-    headers:{
-        "Authorization": `Bearer ${SECRET}`,
-        "Content-Type": "application/json",
-        "api-key": `${APIKEY}`
-    }
-})
+  baseURL: "https://api.xixapay.com/api/v1",
+  headers: {
+    "Authorization": `Bearer ${SECRET}`,
+    "Content-Type": "application/json",
+    "api-key": APIKEY,
+  },
+});
